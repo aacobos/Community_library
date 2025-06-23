2bf984ddf27a924401ecc58b1b9308fc1efdc138
Ariel <arielacobos@hotmail.com>
2025-06-23 06:18:26 -0300

    feat: Environment variables (.env)

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken"
+import "dotenv/config"
 
 function generateJWT(id) {
-    return jwt.sign({id}, "87e71f18096f5ce5e56fdaa9ba8c2d09866b110ad67fa20c86112f5489595f08", {expiresIn: 86400})
+    return jwt.sign({id}, process.env.SECRET_JWT, {expiresIn: 86400})
 }
 
 export { generateJWT }
