d7cc65cb40480f33817448795ade124a5edd8627
Ariel <arielacobos@hotmail.com>
2025-07-14 05:37:55 -0300

    feat: CRUD ebooks

--- src/controller/book.controllers.js
+++ src/controller/book.controllers.js
@@ -17,11 +17,54 @@ async function findAllBooksController(req, res) {
         const books = await bookService.findAllBooksService();
         res.send(books);
     } catch (error) {
-        res.status(400).send(error.message);
+        res.status(404).send(error.message);
     }    
+}
+
+async function findBookByIdController(req, res) {
+    const bookId = req.params.id;
+
+    try {
+        const book = await bookService.findBookByIdService(bookId);
+        return res.send(book);
+    } catch (error) {
+        return res.status(404).send(error.message);
+    }
+}
+
+async function updateBookController(req, res) {
+    const updatedBook = req.body;
+    const bookId = req.params.id;
+    const userId = req.userId;
+
+    try {
+        const response = await bookService.updateBookService(
+            updatedBook,
+            bookId,
+            userId
+        );
+        return res.send(response);
+    } catch (error) {
+        return res.status(400).send(error.message);
+    }
+}
+
+async function deleteBookController(req, res) {
+    const bookId = req.params.id;
+    const userId = req.userId;
+
+    try {
+        const response = await bookService.deleteBookService(bookId, userId);
+        return res.send(response);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 }
 
 export default {
     createBookController,
-    findAllBooksController
+    findAllBooksController,
+    findBookByIdController,
+    updateBookController,
+    deleteBookController
 }
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,6 +1,6 @@
 import bookController from "../controller/book.controllers.js";
 import { Router } from "express";
-import { validate } from "../middlewares/validation.middleware.js";
+import { validate, validateBookId } from "../middlewares/validation.middleware.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { bookSchema } from "../schema/book.schema.js";
 
@@ -10,5 +10,9 @@ router.get("/books", bookController.findAllBooksController);
 
 router.use(authMiddleware);
 router.post("/books", validate(bookSchema), authMiddleware, bookController.createBookController);
+
+router.get("/books/:id", validateBookId, bookController.findBookByIdController);
+router.patch("/books/:id", authMiddleware, validateBookId, bookController.updateBookController);
+router.delete("/books/:id", validateBookId, bookController.deleteBookController);
 
 export default router
