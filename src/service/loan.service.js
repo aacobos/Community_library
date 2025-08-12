9ca0a5d3c0d0767b4aae903f97176f42f04b45d2
Ariel <arielacobos@hotmail.com>
2025-08-12 05:22:07 -0300

    feat: Loan e Delete

---
+++ src/service/loan.service.js
@@ -0,0 +1,33 @@
+import loanRepositories from "../repositories/loan.repositories.js";
+
+async function createLoanService(userId, bookId, dueDate) {
+    const createLoan = await loanRepositories.createLoanRepository(userId, bookId, dueDate)
+    if (!createLoan) throw new Error("Error creating Loan")
+    return createLoan
+}
+
+async function findAllLoansService() {
+    const loans = await loanRepositories.findAllLoansRepository()
+    return loans
+}
+
+async function findLoanByIdService(loanId) {
+    const loan = await loanRepositories.findLoanByIdRepository(loanId)
+    if (!loan) throw new Error("Loan not found")
+    return loan
+}
+
+async function deleteLoanService(loanId, userId) {
+    const loan = await loanRepositories.findLoanByIdRepository(loanId)
+    if (!loan) throw new Error("Loan not found")
+    if (loan.userId !== userId) throw new Error("Unauthorized")
+    const response = await loanRepositories.deleteLoanRepository(loanId)
+    return response
+}
+
+export default {
+    createLoanService,
+    findAllLoansService,
+    findLoanByIdService,
+    deleteLoanService
+}
